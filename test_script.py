44a75a2d37503aa0c0f3184a2471307891c49d65
thejammerr <jaepark.g@gmail.com>
2022-05-17 16:25:51 -0400

    add periodic boundary checking

--- test_script.py
+++ test_script.py
@@ -1,5 +1,4 @@
 from itertools import count
-from turtle import numinput
 import netCDF4 as nc
 import collections
 import numpy as np
@@ -16,8 +15,8 @@ vpos = ds.variables['pos'][:]
 vpos_x = vpos[num_it][::2]
 vpos_y = vpos[num_it][1::2]
 cell_pos = ds['cellPositions'][:] # 7 rows x 400 columns (200 pairs of x,y)
-
-print(vpos.shape)
+box_side_len = ds.variables['BoxMatrix'][0][0];
+print(ds.variables['BoxMatrix'][6])
 
 
 fig, ax = plt.subplots()
@@ -26,21 +25,36 @@ ax.scatter(cell_pos[num_it][::2], cell_pos[num_it][1::2],  c='tab:blue', alpha=0
 # TODO: plot lines connecting each vertex
 curr = 0
 
+def tuple_add(tuple1, tuple2):
+    return tuple(map(lambda x, y: x + y, tuple1, tuple2))
+
+def tuple_sub(tuple1, tuple2):
+    return tuple(map(lambda x, y: x - y, tuple1, tuple2))
 '''
 Notes: vertex indices start from 0 and go to num-1
 '''
 for i in range(0, 1200, 3):
-    v1 = Vneighs[num_it][i]
-    v2 = Vneighs[num_it][i+1]
-    v3 = Vneighs[num_it][i+2]
-    cur_p = (vpos_x[curr], vpos_y[curr])
-    p1 = (vpos_x[v1], vpos_y[v1])
-    p2 = (vpos_x[v2], vpos_y[v2])
-    p3 = (vpos_x[v3], vpos_y[v3])
-    #print(v1)
-    lines = [[cur_p, p1], [cur_p, p2], [cur_p, p3]]
-    lc = mc.LineCollection(lines, linewidths=1)
-    ax.add_collection(lc)
+    for j in range(0, 3):
+        v = Vneighs[num_it][i+j]
+        cur_p = (vpos_x[curr], vpos_y[curr])
+        p = (vpos_x[v], vpos_y[v])
+        line = []
+        point_diff = np.subtract(np.asarray(cur_p), np.asarray(p))
+        if np.linalg.norm(point_diff) < box_side_len/2:
+            line.append([cur_p, p]);
+        else:
+            if point_diff[0] > box_side_len/2:
+                point_diff[0] -= box_side_len
+            if point_diff[0] < -box_side_len/2:
+                point_diff[0] += box_side_len
+            if point_diff[1] > box_side_len/2:
+                point_diff[1] -= box_side_len
+            if point_diff[1] < -box_side_len/2:
+                point_diff[1] += box_side_len
+            line.append([cur_p, tuple_sub(cur_p, point_diff)])
+            line.append([p, tuple_add(p, point_diff)])
+        lc = mc.LineCollection(line, linewidths=1)
+        ax.add_collection(lc)
     curr += 1
 
 plt.show()
