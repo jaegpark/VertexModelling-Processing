dd4794ca6a19e4e5e8b499b9d3f035737bd16e1b
thejammerr <jaepark.g@gmail.com>
2022-06-29 11:00:58 -0400

    debug fixes

--- utils.py
+++ utils.py
@@ -1,7 +1,3 @@
-from tkinter import Toplevel
-from turtle import right
-from types import CellType
-from cv2 import rotate
 import netCDF4 as nc
 import numpy as np
 import matplotlib.pyplot as plt
@@ -13,7 +9,6 @@ from collections import OrderedDict
 import glob
 import cv2
 import os
-from PIL import Image
 
 def get_dataset(fname):
     """
@@ -57,7 +52,7 @@ def seperate_celltype(cellpos, celltypes):
             t2y.append(cellpos[i+1])
     return t1x, t1y, t2x, t2y
 
-def read_files():
+def read_files(file_dir):
     """
     Function that reads an nc file directory where each nc file is a timestamp frame
 
@@ -65,16 +60,16 @@ def read_files():
              data (in order of appearance).  
     """
     frames = OrderedDict()
-    for file in glob.glob("..\data_ME_test\id1\*.nc"):
+    for file in glob.glob(file_dir):
         frames[file] = get_dataset(file)
     return frames
 
 
-def draw_frame(frame_num):
+def draw_frame(frame_num, curr, ax, num_edge, t1x, t1y, t2x, t2y, mesectoderm_vertices):
     """
     
     """
-    global curr, ax, num_edge, t1x, t1y, t2x, t2y, num_cell, mesectoderm_vertices
+    #global curr, ax, num_edge, t1x, t1y, t2x, t2y, num_cell, mesectoderm_vertices
 
     ax.scatter(t1x, t1y,  c='tab:blue', alpha=0.3, edgecolors='none')
     ax.scatter(t2x, t2y,  c='tab:red', alpha=0.3, edgecolors='none')
@@ -237,7 +232,6 @@ def find_mesectoderm_boundary(numv, Vneighs, Vcellneighs, cellType, vposx, vposy
     return set(mes_ver_list), mes_lines
             
 def draw_line(p1, p2, colour, ax):
-    
     """
     draws a line given two coordinate tuples, following the periodic boundary conditions
     """
@@ -268,9 +262,13 @@ def draw_mesectoderm_vertices(vposx, vposy, mes_vertices, ax):
     ax.scatter(x, y, c="tab:green", s=0.1)
     return x, y
 
+def convert_pixel_to_coord(row, col):
+
+    pass
+
 def sweeper(img_path):
     """
-    sweeper takes in an image and returns groupings of pixels corresponding to the top/bottom boundary of the mesectoderm
+    sweeper takes in an image and returns lists of pixels corresponding to the top/bottom boundary of the mesectoderm
     """
     im = cv2.imread(img_path)
     im_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
@@ -291,18 +289,71 @@ def sweeper(img_path):
         # THIS METHOD DOES NOT WORK... WILL INSTEAD TAKE TOPMOST POINT AND BOTTOMMOST POINT      
         #upper = np.average(column[:len(column)//2], axis=1)
         #lower = np.average(column[len(column)//2:], axis=1)
+        if len(column) == 0:
+            '''
+                If the column is empty, then we want to use an interpolated function. 
+                On the first pass, we can insert a 0 for now so that we can search and replace later.
+            '''
+            column.append(0)
+            column.append(0)
+
         upper = column[0]
         lower = column[-1]
         upper_boundary.append(upper)  # list of points, i'th list contains all points corresponding to the i'th column in the image
         lower_boundary.append(lower)
 
-    cv2.imwrite('otsu.jpg', im_gray_th)
+
+    #cv2.imwrite('otsu.jpg', im_gray_th)
+    '''
+    Interpolate missing columns by:
+        get previous filled column point
+        get next filled column point
+        find the slope between the two points
+        add one slope to the previous point, and treat that as your new point
+        TODO: make the case for where the empty column occurs before any existing column 
+                i.e. on the LHS of the screen - in that case, just use the future step as the main one.
+                currently crashes on image 96 of sample pid 1
+    '''
+    if upper_boundary[0] == 0:
+        future = []
+        for i in range(width):
+            if upper_boundary[i] != 0:
+                future = upper_boundary[i]
+                break
+        upper_boundary[0] = future
+    
+    for i in range(width):
+        if upper_boundary[i] == 0:
+            future = []
+            previous = upper_boundary[i-1]
+            for j in range(i, width):
+                if upper_boundary[j] != 0:
+                    future = upper_boundary[j]
+                    break
+            if len(future) == 0:    # unable to find a future point
+                upper_boundary[i] = previous
+            else:
+                upper_boundary[i] = [i, previous[1] + (future[1]-previous[1])/(future[0]-previous[0])]
+        if lower_boundary[i] == 0:
+            future = []
+            previous = lower_boundary[i-1]
+            for j in range(i, width):
+                if lower_boundary[j] != 0:
+                    future = lower_boundary[j]
+                    break
+            if len(future) == 0:    # unable to find a future point
+                lower_boundary[i] = previous
+            else:
+                lower_boundary[i] = [i, previous[1] + (future[1]-previous[1])/(future[0]-previous[0])]
+        
     return upper_boundary, lower_boundary
     
 def step_func(val):
+    """Helper step function for mesectoderm internalization calculation"""
     return 1 if val <= 3 else 0
 
-def calc_pixel_widths(upper_tuple_list, lower_tuple_list, N, width):
+def calc_pixel_widths(upper_tuple_list, lower_tuple_list, width):
+    """Calculates the pixel widths between the top list and bottom list"""
     distances = []
     for i in range(width):
         mean_top = np.mean(upper_tuple_list[i])
@@ -311,36 +362,38 @@ def calc_pixel_widths(upper_tuple_list, lower_tuple_list, N, width):
     
     return distances
 
-def calc_roughness(rotated_points):
-    numpixels = len(rotated_points)
-    '''
-    First, find h bar
-    '''
-    # h-bar calculations:
-    xaxis = rotate_points[0][1]
-    h_tot = 0
-    for i in range(len(rotated_points)):
-        h_tot += np.abs(rotated_points[i][1] - xaxis)
-    h_bar = h_tot / numpixels
+def calc_roughness(rotated_points, segment_size):
+    """
+    Calculates roughness equation from this paper: https://www.sciencedirect.com/science/article/pii/S2667290121000553 
+    return : the mean roughness value over the entire rotated boundary over all segmnets    
+    """
+    num_segments = len(rotated_points)//segment_size
+    xaxis = rotated_points[0][1]    # takes the y value (idx = 1) of the first coordinate point 
+    roughnesses = []
+    for i in range(num_segments):
+        segment = rotated_points[i*segment_size:(i+1)*segment_size]
+        h_tot = 0
+        for j in range(segment_size):
+            h_tot += np.abs(segment[j][1] - xaxis)   # difference in y value
+        h_bar = h_tot / segment_size
+        temp = 0
+        for j in range(segment_size):
+            h_i = np.abs(segment[j][1] - xaxis)      # difference in y value
+            temp += np.power(h_i-h_bar, 2)
+        w2 = temp/segment_size
+        roughnesses.append(np.sqrt(w2))
 
-    '''
-    Now find w^2
-    '''
-    temp = 0
-    for i in range(len(rotated_points)):
-        h_i = np.abs(rotated_points[i][1] - xaxis)
-        temp += np.power(h_i-h_bar, 2)
-    w2 = temp/numpixels
-
-    return np.sqrt(w2)
+    return np.mean(roughnesses)
     
 def calc_mes_internalization(upper_list, lower_list):
+    """Calculates rate of mesectoderm internalization given the upper and bottom mesectoderm points"""
     distances = np.delete(np.subtract(upper_list, lower_list), 0, 1)
     s = [step_func(x) for x in distances]
     ans = np.sum(s)/len(upper_list)
     return ans
 
 def rotate_points(upper_points_list, bottom_points_list, N):
+    """ Rotates a given set of points to align each with its own axis"""
     top_left = upper_points_list[0]
     top_right = upper_points_list[-1]
     bot_left = bottom_points_list[0]
@@ -348,7 +401,7 @@ def rotate_points(upper_points_list, bottom_points_list, N):
    
     #print(top_left[0], top_left[1], top_right[0], top_right[1])
     slope_top = (top_right[1] - top_left[1])/(top_right[0] - top_left[0])
-    print(slope_top)
+    #print(slope_top)
     slope_bot = (bot_right[1] - bot_left[1])/(bot_right[0] - bot_left[0])
     theta_r_top = np.arctan(slope_top)
     theta_r_bot = np.arctan(slope_bot)
@@ -371,61 +424,114 @@ def rotate_points(upper_points_list, bottom_points_list, N):
 
 if __name__ == "__main__":
     num_it = 0
-    fn = read_files()
-    
-    for i in range(0, 1):
-        frame = fn.popitem(False)
-        ds = frame[1]       # fn.popitem(False) returns a (key, value) pair of the first element (FIFO order). [1] grabs the values
-        num_cell = ds.dimensions['Nc'].size
-        num_v = ds.dimensions['Nv'].size
+    fn = read_files("..\celldiv2.8-FIG4DATA\SLOWcelldivON\cellGPU_mesectoderm-ectoderm_02082021\data\*.nc")
+
+    roughness_graph = []
+    read_table = [1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    for i in range(0, len(read_table)):
+        if read_table[i] == 1 and i == 0:
+            roughness_graph = np.append([roughness_graph], np.array([np.genfromtxt('roughness_graph_' + str(10) + '.csv', delimiter=',')]).T)
+            for j in range(0, 120):
+                k = fn.popitem(False)
+                if j == 0 : print(k[0][-20:-1])
+            #print(roughness_graph)
+
+        elif read_table[i] == 1 and i != 0:
+            roughness_graph = np.append([roughness_graph], np.genfromtxt('roughness_graph_' + str(i+1) + '.csv', delimiter=',').T)
+            print("guh")
+            for j in range(0, 120):
+                k = fn.popitem(False)
+                if j == 0 : print(k[0][-20:-1])
+
+    roughness_graph = np.reshape(roughness_graph, (1, -1))    
+    print(roughness_graph.shape)
+    for j in range(1, 2):
+        pid_rough = []
+        pid_name = ""
+        for i in range(0, 120):
+            frame = fn.popitem(False)
+            if i == 0 : print(frame[0][-20:-1])
+            pid_name = frame[0][-20:-14]
+            print(i)
+            ds = frame[1]       # fn.popitem(False) returns a (key, value) pair of the first element (FIFO order). [1] grabs the values
+            num_cell = ds.dimensions['Nc'].size
+            num_v = ds.dimensions['Nv'].size
+            
+            VCellNeighs = ds.variables['VertexCellNeighbors'][:][0] # size of 3 * num_v : groups of 3 (3 cell neighbours for each vertex)
+            cellVertices = np.reshape(ds.variables['cellVer'][:][0], (-1, 16))  # 2D array: i-th row has the list of vertex indices of the i-th cell 
+            cellVerNum = ds.variables['cellVerNum'][:]
+            Vneighs = ds.variables['Vneighs'][:] 
+            num_edge = Vneighs.shape[1]
+            vpos = ds.variables['pos'][:]
+            vpos_x = vpos[num_it][::2]
+            vpos_y = vpos[num_it][1::2]
+            cell_pos = ds['cellPositions'][:] 
+            cell_types = ds['cellType'][num_it]
+            box_side_len = ds.variables['BoxMatrix'][0][0]
+            curr = 0
+            fig, ax = plt.subplots()
+
+            #print(len(cell_types))
+            #print(num_cell)
+            ''' Get Processed Data '''
+            #mesectoderm_cells = get_mesectoderm_cell_indices(numcell=num_cell, celltypelist=cell_types)
+            #mesectoderm_vertices = get_mesectoderm_vertex_indices(num_v, mesectoderm_cells, VCellNeighs) 
+            mesectoderm_boundary_vertices, mesectoderm_boundary_lines = find_mesectoderm_boundary(num_v, Vneighs, VCellNeighs, cell_types, vpos_x, vpos_y)
+            #mesectoderm_vertex_coords_x , mesectoderm_vertex_coords_y = get_mesectoderm_vertex_coords(mesectoderm_vertices, vpos_x, vpos_y, ax)
+
+
+            #t1x, t1y, t2x, t2y = seperate_celltype(cell_pos[num_it], ds.variables['cellType'][num_it])
+            
+
+            #draw_frame(0)   ## TODO: Edit parameters to draw the frame, added globals inside params
         
-        VCellNeighs = ds.variables['VertexCellNeighbors'][:][0] # size of 3 * num_v : groups of 3 (3 cell neighbours for each vertex)
-        cellVertices = np.reshape(ds.variables['cellVer'][:][0], (-1, 16))  # 2D array: i-th row has the list of vertex indices of the i-th cell 
-        cellVerNum = ds.variables['cellVerNum'][:]
-        Vneighs = ds.variables['Vneighs'][:] 
-        num_edge = Vneighs.shape[1]
-        vpos = ds.variables['pos'][:]
-        vpos_x = vpos[num_it][::2]
-        vpos_y = vpos[num_it][1::2]
-        cell_pos = ds['cellPositions'][:] 
-        cell_types = ds['cellType'][num_it]
-        box_side_len = ds.variables['BoxMatrix'][0][0];
-        curr = 0
-        fig, ax = plt.subplots()
+            #bcoords_x, bcoords_y = draw_mesectoderm_vertices(vpos_x, vpos_y, mesectoderm_boundary_vertices, ax)    # ALSO DRAWS THE BOUNDARY
+            plt.axis([0, 20, 8, 12])
+            for lines in mesectoderm_boundary_lines:
+                draw_line(lines[0], lines[1], 'tab:green', ax)
+            
+            plt.axis('off')
+            ax.get_xaxis().set_visible(False)
+            ax.get_yaxis().set_visible(False)
 
-        #print(len(cell_types))
-        #print(num_cell)
-        ''' Get Processed Data '''
-        mesectoderm_cells = get_mesectoderm_cell_indices(numcell=num_cell, celltypelist=cell_types)
-        mesectoderm_vertices = get_mesectoderm_vertex_indices(num_v, mesectoderm_cells, VCellNeighs) 
-        mesectoderm_boundary_vertices, mesectoderm_boundary_lines = find_mesectoderm_boundary(num_v, Vneighs, VCellNeighs, cell_types, vpos_x, vpos_y)
-        mesectoderm_vertex_coords_x , mesectoderm_vertex_coords_y = get_mesectoderm_vertex_coords(mesectoderm_vertices, vpos_x, vpos_y, ax)
-
-
-        t1x, t1y, t2x, t2y = seperate_celltype(cell_pos[num_it], ds.variables['cellType'][num_it])
+            #plt.show()
+            #print(frame[0][-20:-3])
+            filename = '../frame_images/{fname}.png'.format(fname = frame[0][-20:-3])
+            plt.savefig(filename, bbox_inches='tight', pad_inches=0)
+            plt.close('all')
+            upperboundary, lowerboundary = sweeper(filename)
+            r_upper_boundary, r_lower_boundary = rotate_points(upper_points_list=upperboundary, bottom_points_list=lowerboundary, N=1)
+            #print(r_upper_boundary[0:40])
+            roughness_of_timeframe = calc_roughness(r_upper_boundary, 150)
+            pid_rough.append(roughness_of_timeframe)
+            ##print(roughness)
+        if len(roughness_graph) == 0: 
+            roughness_graph = [pid_rough]
+        else:
+            np.concatenate((roughness_graph, [pid_rough]), axis=0)
+            print(roughness_graph.shape)
         
+        name = j if j != 0 else 10
+        np.savetxt("roughness_graph_" + str(name) + ".csv", pid_rough, delimiter=",")
 
-        #draw_frame(0)
-     
-        bcoords_x, bcoords_y = draw_mesectoderm_vertices(vpos_x, vpos_y, mesectoderm_boundary_vertices, ax)    # ALSO DRAWS THE BOUNDARY
-        plt.axis([0, 20, 8, 12])
-        for lines in mesectoderm_boundary_lines:
-            draw_line(lines[0], lines[1], 'tab:green', ax)
-        
-        plt.axis('off')
-        ax.get_xaxis().set_visible(False)
-        ax.get_yaxis().set_visible(False)
-
-        #plt.show()
-        filename = '../frame_images/{fname}.png'.format(fname = frame[0][20:-3])
-        plt.savefig(filename, bbox_inches='tight', pad_inches=0)
+        print("pid {j} done".format(j=name))
 
     #convert_img_to_mov('../frame_images/', '../videos/test_1.avi')
-
+    
+    roughness_plot = np.mean(roughness_graph, 0)
+    
+    fig = plt.figure()
+    plt.plot(np.linspace(1, 120, 120), roughness_plot, 'o')
+    plt.savefig('../roughness_graph_SLOW.png', bbox_inches='tight', pad_inches=0)
+    plt.show()
     '''
         TODO: 
-        3. find a way to convert the saved images and the coordinates...
+        1. fix bug with memory 
+            a) can do batch runs on each p_id and save the values in some data file, read later
+            b) try diagnosing the matplotlib problem
+                try tracking computer memory during the run
+        2. complete the pipeline to calculate the other values as well
+        3. find unit conversions for y axis - fix in the initial data handling function
     '''
-    upperboundary, lowerboundary = sweeper(filename)
-    r_upper_boundary, r_lower_boundary = rotate_points(upper_points_list=upperboundary, bottom_points_list=lowerboundary, N=1)
+
     
